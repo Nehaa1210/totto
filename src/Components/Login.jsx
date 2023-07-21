452545de31185a3e73aa0eb811c27ef1276dd11d
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-07-22 01:02:23 +0530

    day4

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,21 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+
+const handleform = (e) => {
+  e.preventDefault()
+
+}
+const handleemail = (e) => {
+  console.log(e.target.value)
+}
+const handlepassword = (e) => {
+  console.log(e.target.value)
+   
+}
+
+
   return (
     <div className="container">
-      <form className="login-form" role="search">
+      <form className="login-form" role="search" onChange={(e)=>handleform(e)}>
         <h2>Login Now!</h2>
         <input
           className="form-control me-2"
           type="search"
           placeholder="Email"
           aria-label="Search"
+          onChange={(e)=>handleemail(e)}
         />
         <input
           className="form-control me-2"
           type="search"
           placeholder="Password"
           aria-label="Search"
+          onChange={(e)=>handlepassword(e)}
+
         />
         <button className="btn btn-success" type="submit">
           Login
